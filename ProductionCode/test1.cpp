6dec1297c3c98de3311c5a09a91d989a9847b1a2
LaizFigueroa <laizheck@hotmail.com>
2018-02-26 12:19:37 +0100

    The best branch ever

--- ProductionCode/test1.cpp
+++ ProductionCode/test1.cpp
@@ -1,29 +1,37 @@
-#include <chrono>
 #include <iostream>
+#include <chrono>
 
 #include "cluon/UDPSender.hpp"
 #include "cluon/UDPReceiver.hpp"
-#include "PrimeChecker.hpp"
 
 int main(int /*argc*/, char** /*argv*/) {
-    PrimeChecker pc;
-    std::cout << "Hello World = " << pc.isPrime(43) << std::endl;
-
-    cluon::UDPSender sender{"225.0.0.111", 1236};
-    sender.send("Hello World!");
 
     using namespace std::literals::chrono_literals;
     std::this_thread::sleep_for(5s);
+    //std::cout << "Operational " << std::endl;
 
-    cluon::UDPReceiver receiver("225.0.0.111", 1236,
+cluon::UDPReceiver receiver("225.0.0.111", 1236,//"127.0.0.1",1234
         [](std::string &&data, std::string &&/*from*/,
-           std::chrono::system_clock::time_point &&/*timepoint*/) noexcept {
+           std::chrono::system_clock::time_point &&/*timepoint*/) 
+
+noexcept {
         std::cout << "Received " << data.size() << " bytes." << std::endl;
+	 std::cout << "Received " <<  data << std::endl;
     });
 
+
+	cluon::UDPSender sender{"225.0.0.111", 1236};
+    sender.send("Testing");
+
+
     using namespace std::literals::chrono_literals;
+
     while (receiver.isRunning()) {
+
         std::this_thread::sleep_for(1s);
+
     }
+
     return 0;
+
 }
