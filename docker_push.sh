995f96fbe3f3432cd8b6f3af9a4fc2a21de7d088
LaizFigueroa <gushecla@student.gu.se>
2018-03-27 11:08:06 +0200

    Pushing the tagged image to Docker hub

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker tag dit168 "$DOCKER_USERNAME"/dit168:Auto
-docker push "$DOCKER_USERNAME"/dit168
+docker push "$DOCKER_USERNAME"/dit168:Auto
