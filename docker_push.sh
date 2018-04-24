045476473f25c58e35de795575c716c5fb067f90
LaizFigueroa <gushecla@student.gu.se>
2018-04-24 09:38:44 +0200

    Some changes to test if travis work

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker tag dit168 "$DOCKER_USERNAME"/dit168
-docker push "$DOCKER_USERNAME"/dit168
+docker tag v2v "$DOCKER_USERNAME"/v2v
+docker push "$DOCKER_USERNAME"/v2v
+
+docker tag app "$DOCKER_USERNAME"/app
+docker push "$DOCKER_USERNAME"/app
+
+docker tag interceptor "$DOCKER_USERNAME"/interceptor
+docker push "$DOCKER_USERNAME"/interceptor
