3c9bb966215f99a4319df2ffb07d2d3bc524771d
LaizFigueroa <gushecla@student.gu.se>
2018-04-24 17:27:03 +0200

    Adding scripts

--- docker_pushInterceptor.sh
+++ docker_pushInterceptor.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker tag v2v "$DOCKER_USERNAME"/v2v
-docker push "$DOCKER_USERNAME"/v2v
-
-docker tag app "$DOCKER_USERNAME"/app
-docker push "$DOCKER_USERNAME"/app
-
 docker tag interceptor "$DOCKER_USERNAME"/interceptor
 docker push "$DOCKER_USERNAME"/interceptor
