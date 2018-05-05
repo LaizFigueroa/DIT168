cf172ff4616f5d01d1b3e93abdfec7f5f83e666f
LaizFigueroa <gushecla@student.gu.se>
2018-05-05 08:21:53 +0200

    Fixing names

--- scripts/docker_pushImu.sh
+++ scripts/docker_pushImu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker tag imu "$DOCKER_USERNAME"/imu
-docker push "$DOCKER_USERNAME"/imu
+docker tag imu_receiver "$DOCKER_USERNAME"/imu_receiver
+docker push "$DOCKER_USERNAME"/imu_receiver
