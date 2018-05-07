505ea6cacf2fe0ee73dee4069e0216c6dde87822
Rema-Salman <gussalmre@student.gu.se>
2018-05-07 14:27:16 +0200

    adding comments, references and od4session for web-visualisation

--- src/app-server/Server.cpp
+++ src/app-server/Server.cpp
@@ -13,6 +13,10 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+// Created by Rema Salman on 2018-04-28.
+//
+// Refrences used : - The UDP server is, avalible on: <https://www.cs.rutgers.edu/~pxk/417/notes/sockets/udp.html>
+//                  - Splitting the buffer message, avalible on: <https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c>
 
 #include <iostream>
 #include <string.h>
@@ -29,107 +33,115 @@
 #include "messages.hpp"
 
 
-using namespace std;
+using namespace std; // Name space declaration
+/* Variabuls declaration*/
 int SOCKET_BUFFER_SIZE = 1024;
 socklen_t *addrlen;
-int on=1;
 string speed;
-string angle;
+string angle;  // the message that contains the full commands
+string message = "";
 
- cluon::OD4Session od4(240, [](cluon::data::Envelope && envelope) noexcept {
+const int delay = 100; // Delay for sending the messages
+static const int COMMANDS_CHANNEL    = 242;
+static const int INTERNAL_CHANNEL    = 240;
 
-                });
+cluon::OD4Session od4(COMMANDS_CHANNEL, [](cluon::data::Envelope && envelope) noexcept {});
+
+/* The main executing of the program */
 int main() {
-    sockaddr_in server_addr;
-    int server;
-    socklen_t size = sizeof(server_addr);
-    server = socket(AF_INET, SOCK_DGRAM, 0);
+
+sockaddr_in server_addr;
+int server;
+socklen_t size = sizeof(server_addr);
+server = socket(AF_INET, SOCK_DGRAM, 0);
     
-    if (server == -1) {
-        cout << "\n Error establishing socket..." << endl;
-        exit(1);
-    }
     
-    cout << "\n=> Socket server has been created..." << endl;
-    server_addr.sin_family = AF_INET;
-    //10.132.27.127
-    server_addr.sin_addr.s_addr = inet_addr("172.20.10.2");
-    server_addr.sin_port = htons(8080);
-    bind(server, (sockaddr * ) & server_addr, size);
-    
-    /**
-     * this while loop continously listening to data from the java client
-     */
-    while (1) {
-        int command;
-        int returnvalue=0;
-        char buffer[SOCKET_BUFFER_SIZE];
-        size_t pos = 0;
+if (server == -1) {
+    cout << "\n Error establishing socket..." << endl;
+    exit(1);
+}
 
-        // the message that contains the command
-        string message = "";
+cout << "\n=> Socket server has been created..." << endl;
+//Set up the socket IP family to AF_INET
+server_addr.sin_family = AF_INET;
+// Socket's IP address
+server_addr.sin_addr.s_addr = inet_addr("172.20.10.2");
+// Port used for transporting the data
+server_addr.sin_port = htons(8080);
+//binding a unique name to the descriptor socket
+bind(server, (sockaddr * ) & server_addr, size);
+// buffer's deceleration
+char buffer[SOCKET_BUFFER_SIZE];
 
-        while (1) {
+/**
+  * This while loop continously listening to data from the java client
+*/
+while (1) {
+    size_t pos = 0  ;
+    size_t array = sizeof(buffer);
+    cout << "before  ..." << endl;
+    // Clear the buffer
+    memset(buffer, 0, sizeof(buffer));
+    // receive function for an incoming datagram
+    recvfrom(server, &buffer, sizeof(buffer), 0, (struct sockaddr *) &server_addr, &size);
+        
+    // Side case failler  of receiving
+    if (recvfrom(server, &buffer, sizeof(buffer), 0, (struct sockaddr *) &server_addr, &size) < 0) {
+        cout << "Fatal: Failed to receive data" << endl;
+        break;
+    } else {
+        // Checking the buffer when receiving
+        buffer[strlen(buffer)] = '\0';
+        char* end_of_buffer = buffer;
+        size_t remaining_space = sizeof(buffer);
+        message=buffer;
+        cout << "the incoming stream : " << message << endl;
 
-            char * element;
-            size_t i;
-            size_t array = sizeof(buffer);
+        // Separating the string command into 2 strings angle and speed
+        string delimiter = "//";
+        while ((pos = message.find(delimiter)) != string::npos) {
+            angle = message.substr(0, pos);
+            cout << angle << endl;
+            message.erase(0, pos + delimiter.length());
+            speed =message;
+            cout << "message is: " << angle << "/" << speed <<  endl;
+        }
+        // clear the buffer
+        memset(buffer, 0, array);
+        // converting received string to float
+        float anglef = stof(angle);
+        float speedf = stof(speed);
+        
+        // Declaring and initializing the internal OD4session
+        cluon::OD4Session internal(INTERNAL_CHANNEL, [](cluon::data::Envelope && envelope) noexcept {});
 
-            cout << "before  ..." << endl;
-            memset(buffer, 0, sizeof(buffer));
+        // In case, nither OD4Session is not working
+        if (od4.isRunning() == 0) {
+            cout << "ERROR: No od4 session is running!!!" << endl;
+            return -1;
+        }
+            
+         else if (internal.isRunning() == 0){
+            cout << "ERROR: No internal session is running!!!" << endl;
+            return -1;
 
-            if (recvfrom(server, &buffer, sizeof(buffer), 0, (struct sockaddr *) &server_addr, &size) < 0) // recvfrom() function call
-            {
-                cout << "Fatal: Failed to receive data" << endl;
-                break;
-            } else
-
-                cout << "=> start ..." << endl;
-            cout << buffer << endl;
-            buffer[strlen(buffer)] = '\0';
-            char* end_of_buffer = buffer;
-            std::size_t remaining_space = sizeof(buffer);
-
-            message=buffer;
-            //message.append(buffer);
-            cout << "the incoming stream : " << message << endl;
-
-            std::string delimiter = "//";
-            while ((pos = message.find(delimiter)) != std::string::npos) {
-
-                angle = message.substr(0, pos);
-                std::cout << angle << std::endl;
-                message.erase(0, pos + delimiter.length());
-                speed =message;
-                cout << "message isa now: " << angle << "/" << speed <<  endl;
-
-            }
-            memset(buffer, 0, array);
-           // std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
+         }else{
+            // Specify the messages as in messages.odvd
+            opendlv::proxy::GroundSteeringReading msgSteering;
+            opendlv::proxy::PedalPositionReading msgPedal;
                 
-                // converting string to float
-                float anglef = stof(angle);
-                float speedf = stof(speed);
-               
-                
-                if (od4.isRunning() == 0) {
-                    std::cout << "ERROR: No od4 running!!!" << std::endl;
-                    return -1;
-                }
-                
-                opendlv::proxy::GroundSteeringReading msgSteering;
-                opendlv::proxy::PedalPositionReading msgPedal;
-                const int delay = 100;
-                
-                // HANDELING MOVMENT
-                std::cout << " EXECUTING SOME MOVEMENTS ..." << std::endl;
-                msgSteering.steeringAngle(anglef);
-                od4.send(msgSteering);
-                msgPedal.percent(speedf);
-                od4.send(msgPedal);
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                
+            // HANDELING MOVMENTS: forward sending the commands received, via the commands OD4Session then to the web-visualizaation internal OD4session
+            cout << " EXECUTING SOME MOVEMENTS ..." << endl;
+            msgSteering.steeringAngle(anglef);
+            od4.send(msgSteering);
+            internal.send(msgSteering);
+            
+            msgPedal.percent(speedf);
+            od4.send(msgPedal);
+            internal.send(msgPedal);
+            // A delay for the messages' thread
+            this_thread::sleep_for(chrono::milliseconds(delay));
             }
         }
     }
+}
